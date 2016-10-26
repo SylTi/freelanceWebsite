5525050655f7c2dd4d21660ca75e475a9de7fb9b
Sylvain <sylvain.tissier@gmail.com>
2016-10-26 18:38:38 +0200

    Add analytics to blog

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import "rxjs/add/operator/distinctUntilChanged";
+
+declare var ga: any;
 
 @Component({
     selector: 'app-root',
@@ -25,6 +28,20 @@ export class AppComponent {
                     if (element) { element.scrollIntoView(element); }
                 }
             }
+        });
+
+        // send pageview to Google Analytics
+        ga('create', 'UA-86335924-1', 'auto');
+        setTimeout(function(){ga('send', '_trackEvent', 'Control', 'Bounce Rate', '')}, 20000);
+
+        router.events.distinctUntilChanged((previous: any, current: any) => {
+            if(current instanceof NavigationEnd) {
+                return previous.url === current.url;
+            }
+            return true;
+        }).subscribe((x: any) => {
+            console.log('send pageview');
+            ga('send', 'pageview', x.url);
         });
     }
 
--- site/assets/js/custom.js
+++ site/assets/js/custom.js
@@ -250,3 +250,19 @@
 	});
 
 })(jQuery);
+
+// Google Analytics
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-86335924-1', 'auto');
+ga('send', 'pageview');
+
+// No longer bounce after 20s on the page
+setTimeout(function(){
+	// _gaq.push(['_trackEvent', 'Control', 'Bounce Rate', ''])
+	ga('send', '_trackEvent', 'Control', 'Bounce Rate', '');
+}, 20000);
